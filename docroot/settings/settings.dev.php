46b60b85db3c6c57267a53e51dd3d8dc07cfb44d
Radim Klaska <radim@klaska.net>
2015-09-20 16:41:22 +0200

    Environment-aware settings.php

---
+++ docroot/settings/settings.dev.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * UPDATE TO D8
+ */
+
+//// Environment Indicator.
+//$conf['environment_indicator_color'] = 'green';
+//$conf['environment_indicator_text'] = 'DEV ENVIRONMENT';
+//
+//// Turn off mail.
+//$conf['mail_system'] = array(
+//  'default-system' => 'DevelMailLog',
+//);
---
+++ docroot/settings/settings.test.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * UPDATE TO D8
+ */
+
+//// Environment.
+//$conf['environment_indicator_color'] = 'blue';
+//$conf['environment_indicator_text'] = 'TEST ENVIRONMENT';
+//
+//// Turn off mail.
+//$conf['mail_system'] = array(
+//  'default-system' => 'DevelMailLog',
+//);
