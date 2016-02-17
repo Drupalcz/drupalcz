3c98c40337534a553aec9c0ed52f094405cd5f4f
Radim Klaska <radim@klaska.net>
2016-02-17 04:43:39 +0100

    Add default DB credentials to settings.travis.php.

--- docroot/sites/default/settings.travis.php
+++ docroot/sites/default/settings.travis.php
@@ -1,5 +1,17 @@
 <?php
 
+// Main DB for D8 install.
+$databases['default']['default'] = array (
+  'database' => 'drupalcz',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
 // Make second DB accessable for travis.
 $databases['migrate']['default'] = array (
   'database' => 'd6migratesource',
