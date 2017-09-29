9130ba9af54a567b99067bb87d64183694f0b863
Radim Klaska <radim@klaska.net>
2017-09-29 13:14:36 +0200

    dcz-74: Remove calls to undefined function on dev and stage. Refs 3090ac8.

--- docroot/sites/default/settings.dev.php
+++ docroot/sites/default/settings.dev.php
@@ -1,8 +1,5 @@
 <?php
 
-// Lock dev.
-secure_the_site_please();
-
 // Turn off mail.
 $conf['mail_system'] = array(
   'default-system' => 'DevelMailLog',
--- docroot/sites/default/settings.test.php
+++ docroot/sites/default/settings.test.php
@@ -1,8 +1,5 @@
 <?php
 
-// Lock test.
-secure_the_site_please();
-
 // Turn off mail.
 $conf['mail_system'] = array(
   'default-system' => 'DevelMailLog',
