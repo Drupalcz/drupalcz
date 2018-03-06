59646659bade2a898afbf7e2b42e072e14271582
Radim Klaska <radim@klaska.net>
2018-03-06 17:28:06 +0100

    Revert "dcz-161: Disable shield on travis." as BLT already uses default.local.settings.php on travis.
    This reverts commit 6758219453efe4538a2b4a4d266ca20ded51533c.

--- docroot/sites/default/settings/travis.settings.php
+++ docroot/sites/default/settings/travis.settings.php
@@ -1,12 +1,4 @@
 <?php
 
-/**
- * Shield.
- */
-$config['shield.settings']['allow_cli'] = TRUE;
-$config['shield.settings']['credentials']['shield']['user'] = '';
-$config['shield.settings']['credentials']['shield']['pass'] = '';
-$config['shield.settings']['print'] = '¯\_(ツ)_/¯';
-
 // Run Travis tests against prod config.
 $config['config_split.config_split.prod']['status'] = TRUE;
