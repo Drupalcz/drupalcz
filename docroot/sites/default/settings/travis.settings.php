43fb387edc7c24b1c45857876f57664dbc9bee76
Karel Majzlík <kmajzlik@users.noreply.github.com>
2018-09-26 09:16:30 +0200

    Fix config path for travis.
    https://github.com/acquia/blt/issues/3086

--- docroot/sites/default/settings/travis.settings.php
+++ docroot/sites/default/settings/travis.settings.php
@@ -2,3 +2,14 @@
 
 // Run Travis tests against prod config.
 $config['config_split.config_split.prod']['status'] = TRUE;
+
+/**
+ * Install profile.
+ *
+ * It needs to be here. Otherwise install process edits this file.
+ */
+$settings['install_profile'] = 'minimal';
+/**
+ * For custom installation
+ */
+$config_directories['sync'] = "../config/default";
