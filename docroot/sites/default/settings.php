38f315bbc8b99b295b9d5d02e7026c8bd73e561f
Radim Klaska <radim@morpht.com>
2018-10-27 18:30:36 +0200

    dcz-134: Workaround for "Undefined index: HTTP_HOST" on Travis.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -5,6 +5,10 @@
  * Drupal site-specific configuration file.
  */
 
+// Workaround for "Undefined index: HTTP_HOST" on Travis.
+// @See: https://github.com/acquia/blt/issues/2527#issuecomment-375690543
+$_SERVER['PWD']=DRUPAL_ROOT;
+
 // BLT will setup lots of things for us.
 // @See: docroot/sites/default/settings/includes.settings.php
 require DRUPAL_ROOT . "/../vendor/acquia/blt/settings/blt.settings.php";
