5df8d3f0e54ab5326cf6b17372286c9c68c48f1c
martin-klima <martin.klima@hqis.cz>
2019-06-10 20:16:57 +0300

    dcz-273: Add APD module and entities.

---
+++ docroot/modules/custom/dcz_apd/src/ApdMembershipAccessControlHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\dcz_apd;
+
+use Drupal\Core\Entity\EntityAccessControlHandler;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+
+/**
+ * Access controller for the APD membership entity.
+ *
+ * @see \Drupal\dcz_apd\Entity\ApdMembership.
+ */
+class ApdMembershipAccessControlHandler extends EntityAccessControlHandler {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
+    /** @var \Drupal\dcz_apd\Entity\ApdMembershipInterface $entity */
+    switch ($operation) {
+      case 'view':
+        if (!$entity->isValid()) {
+          return AccessResult::allowedIfHasPermission($account, 'view invalid apd membership entities');
+        }
+        return AccessResult::allowedIfHasPermission($account, 'view valid apd membership entities');
+
+      case 'update':
+        return AccessResult::allowedIfHasPermission($account, 'edit apd membership entities');
+
+      case 'delete':
+        return AccessResult::allowedIfHasPermission($account, 'delete apd membership entities');
+    }
+
+    // Unknown operation, no opinion.
+    return AccessResult::neutral();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
+    return AccessResult::allowedIfHasPermission($account, 'add apd membership entities');
+  }
+
+}
