ce201c6aeccc0ffb22711ee2e8e4611df38a9855
Miro Michalicka <miro.michalicka@madeit.digital>
2020-10-25 11:00:55 +0100

    Set temp dir path.

--- docroot/sites/default/settings/ac.dev.settings.php
+++ docroot/sites/default/settings/ac.dev.settings.php
@@ -7,3 +7,5 @@ $conf['mail_system'] = array(
 
 $config['config_split.config_split.dev']['status'] = TRUE;
 $config['config_split.config_split.default_content']['status'] = FALSE;
+// @see https://www.drupal.org/project/dropzonejs/issues/2916330
+$settings['file_temp_path'] = "/mnt/gfs/{$_ENV['AH_SITE_GROUP']}.{$_ENV['AH_SITE_ENVIRONMENT']}/tmp";
--- docroot/sites/default/settings/ac.test.settings.php
+++ docroot/sites/default/settings/ac.test.settings.php
@@ -8,3 +8,5 @@ $conf['mail_system'] = array(
 // Keep stage as close to prod as possible.
 $config['config_split.config_split.prod']['status'] = TRUE;
 $config['config_split.config_split.default_content']['status'] = FALSE;
+// @see https://www.drupal.org/project/dropzonejs/issues/2916330
+$settings['file_temp_path'] = "/mnt/gfs/{$_ENV['AH_SITE_GROUP']}.{$_ENV['AH_SITE_ENVIRONMENT']}/tmp";
