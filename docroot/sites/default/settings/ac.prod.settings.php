10142d33e715aa70cc5583b0034b5aedbe66f434
Miro Michalicka <miro.michalicka@madeit.digital>
2020-10-25 11:00:55 +0100

    Set temp dir path.

--- docroot/sites/default/settings/ac.prod.settings.php
+++ docroot/sites/default/settings/ac.prod.settings.php
@@ -5,3 +5,5 @@ $base_url = 'https://www.drupal.cz';
 
 $config['config_split.config_split.prod']['status'] = TRUE;
 $config['config_split.config_split.default_content']['status'] = FALSE;
+// @see https://www.drupal.org/project/dropzonejs/issues/2916330
+$settings['file_temp_path'] = "/mnt/gfs/{$_ENV['AH_SITE_GROUP']}.{$_ENV['AH_SITE_ENVIRONMENT']}/tmp";
