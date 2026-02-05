ba18dcfc06072e2c73cd743258dfd57117dc40b5
Miro Michalicka <miro.michalicka@gmail.com>
2026-02-05 19:42:42 +0100

    Replace npm with yarn for building libraries

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -83,7 +83,8 @@ composer install --prefer-dist --no-progress --no-suggest
 for dir in docroot/libraries/*; do
   if [ -f "$dir/package.json" ]; then
     cd "$dir"
-    npm run build || true
+    yarn install
+    yarn build
     cd -
   fi
 done
